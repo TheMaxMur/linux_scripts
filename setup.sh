86e72afac555672c5df433760681a3a716c448ab
TheMaxMur <maxmur@protonmail.com>
2020-09-23 02:54:38 +0300

    Fix miss in setup

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ sudo mv sources/.pus.sh /usr/bin/
 mv sources/.update.sh ~/
 mv sources/kali_linux_setup.sh ~/
 
-echo 'alise upass='/usr/bin/.pus.sh'' >> ~/.bashrc
+echo 'alias upass='/usr/bin/.pus.sh'' >> ~/.bashrc
