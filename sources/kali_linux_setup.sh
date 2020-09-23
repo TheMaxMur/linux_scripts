43ecf33a093b352debad0773e775f1838719f689
TheMaxMur <maxmur@protonmail.com>
2020-09-23 18:10:14 +0300

    Change script

--- sources/kali_linux_setup.sh
+++ sources/kali_linux_setup.sh
@@ -2,13 +2,17 @@
 
 #add repositories
 
-apt update
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 apt update
 
 #install software
 
-apt install ufw ssh git lnav fail2ban spotify remmina mat gimp openshot psensor audacity vlc filezilla audacious thunderbird neofetch
+apt install ufw ssh git lnav fail2ban spotify-client remmina mat gimp openshot psensor audacity vlc filezilla audacious thunderbird neofetch pass qtpass code torbrowser-launcher 
+wget https://www.vmware.com/go/getplayer-linux
+chmox +x getplayer-linux
+./getplayer-linux
 apt full-upgrade	
 
 #settings 
@@ -29,5 +33,27 @@ echo "Start service"
 service ssh start 
 service ufw start 
 service fail2ban start
+
+echo "Write configs" 
+echo "
+[greeter]
+background = /home/maxmur/Pictures/kJMKLzE.jpg
+theme-name = Kali-Dark
+cursor-theme-name = Adwaita
+xft-antialias = true
+xft-hintstyle = slight
+xft-rgba = rgb
+font-name = Cantarell 11
+icon-theme-name = Flat-Remix-Blue-Dark
+xft-dpi = 96
+indicators = ~host;~spacer;~session;~language;~a11y;~clock;~power;
+clock-format = %d %b, %H:%M
+screensaver-timeout = 60
+default-user-image = #emblem-kali
+position = 5%,start 45%,center
+" > /etc/lightdm/lightdm-gtk-greeter.conf
+echo "Enter path to your .config folder"
+read config_path
+cp -r $config_path ~
 
 echo "Setup is completed"
