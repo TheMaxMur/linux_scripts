e9645c1d9c33fcca784583b0881d33a95304bb34
TheMaxMur <maxmur@protonmail.com>
2020-09-24 01:24:44 +0300

    Add chmod +x

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo 'Move scripts file in folders'
+chmod +x sources/.pus.sh
+chmod +x sources/.update.sh
+chmod +x sources/kali_linux_setup.sh
 sudo mv sources/.pus.sh /usr/bin/
 mv sources/.update.sh ~/
 mv sources/kali_linux_setup.sh ~/
