4b26b3c0e91c08443096441db522157e06a346ac
TheMaxMur <maxmur@protonmail.com>
2020-11-26 22:40:56 +0300

    edit .update.sh and alias

--- setup.sh
+++ setup.sh
@@ -5,7 +5,10 @@ chmod +x sources/.pus.sh
 chmod +x sources/.update.sh
 chmod +x sources/kali_linux_setup.sh
 sudo mv sources/.pus.sh /usr/bin/
-mv sources/.update.sh ~/
+mv sources/.update.sh /usr/bin/.update.sh
 mv sources/kali_linux_setup.sh ~/
 
 echo 'alias upass='/usr/bin/.pus.sh'' >> ~/.bashrc
+sudo echo 'alias upass='/usr/bin/.update.sh'' >> /root/.bashrc
+echo 'alias upgr='/usr/bin/.pus.sh'' >> /root/.bashrc
+sudo echo 'alias upgr='/usr/bin/.update.sh'' >> /root/.bashrc
--- sources/.pus.sh
+++ sources/.pus.sh
@@ -2,4 +2,5 @@
 # alias upass="/usr/bin/.pus.sh"
 
 cd ~/.password-store
+git pull origin master
 git push origin master
